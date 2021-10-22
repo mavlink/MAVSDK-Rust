12a47a7d176ee02132c2ca98fc0d53b1c6104869
daniel.eades <danieleades@hotmail.com>
2021-10-22 15:48:46 +0100

    remove an allocation

--- build.rs
+++ build.rs
@@ -17,12 +17,12 @@ fn main() -> std::io::Result<()> {
 
     for plugin in plugins {
         let proto_path = proto_path(plugin);
-        let proto_include_paths = proto_include_paths(plugin);
+        let proto_include_path = proto_include_path(plugin);
         let proto_out_dir = proto_generated_path(plugin);
         tonic_build::configure()
             .build_server(false)
             .out_dir(proto_out_dir)
-            .compile(&[proto_path], &proto_include_paths)?;
+            .compile(&[proto_path], &[proto_include_path])?;
     }
     Ok(())
 }
@@ -35,6 +35,6 @@ fn proto_generated_path(plugin_name: &str) -> String {
     format!("src/generated/{name}", name = plugin_name)
 }
 
-fn proto_include_paths(plugin_name: &str) -> Vec<String> {
-    vec![format!("proto/protos/{name}", name = plugin_name)]
+fn proto_include_path(plugin_name: &str) -> String {
+    format!("proto/protos/{name}", name = plugin_name)
 }
