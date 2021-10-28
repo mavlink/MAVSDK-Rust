818113a5637364c5ec232f59d3e865257f9507c4
danieleades <33452915+danieleades@users.noreply.github.com>
2021-10-28 11:47:28 +0100

    bump tonic dependencies (#30)

--- src/generated/action/mavsdk.rpc.action.rs
+++ src/generated/action/mavsdk.rpc.action.rs
@@ -189,7 +189,7 @@ pub mod action_service_client {
     impl<T> ActionServiceClient<T>
     where
         T: tonic::client::GrpcService<tonic::body::BoxBody>,
-        T::ResponseBody: Body + Send + Sync + 'static,
+        T::ResponseBody: Body + Send + 'static,
         T::Error: Into<StdError>,
         <T::ResponseBody as Body>::Error: Into<StdError> + Send,
     {
